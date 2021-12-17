ba6bb5854ca5ba46e9688bead12f6fdd6f554ef3
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2021-12-17 19:30:53 +0700

    tribes

---
+++ pages/tribes.tsx
@@ -0,0 +1,37 @@
+
+export default function tribes() {
+  return (
+    <div className="w-4/5 mx-auto">
+      <h2>Tribes of Yoruba</h2>
+      <p>The Yoruba people are a huge </p>
+      <div>
+        <ul>
+          <li>Egba (Ogun state)</li>
+          <li>Ibolo (Osun and Kwara state)</li>
+          <li>Igbonna/Igbomina (Osun, Kwara, Kogi states)</li>
+          <li>Okun (Kogi State)</li>
+          <li>Ijebu</li>
+          <li>Ijebu(Ogun State)</li>
+          <li>Iseyin</li>
+          <li>Owu</li>
+          <li>Anago</li>
+          <li>Anago</li>
+          <li>Awori</li>
+          <li>Yewa</li>
+          <li>Ekiti</li>
+          <li>Ijesa</li>
+          <li>Ilorin</li>
+          <li>Akoko</li>
+          <li>Ilaje</li>
+          <li>Ife</li>
+          <li>Ikare</li>
+          <li>Egun</li>
+          <li>Akoko</li>
+          <li>Otipa</li>
+          {/* https://www.nairaland.com/2270917/list-yoruba-dialects/1 */}
+        </ul>
+        
+      </div>
+    </div>
+  )
+}
